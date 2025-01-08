52a052d998f91a723d556f3bdb5bf086d05a3977
Arjielyn <arjielyn.pogenio@gmail.com>
2025-01-08 20:20:35 +0800

    adding all files

--- answer/src/app/app.routes.ts
+++ answer/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Route1Component } from './route-1/route-1.component';
+import { Route2Component } from './route-2/route-2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Route1Component
+    },
+    {
+        path: 'route_2',
+        component: Route2Component
+    }
+];
---
+++ answer/src/app/route-1/route-1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-1',
+  imports: [],
+  templateUrl: './route-1.component.html',
+  styleUrl: './route-1.component.css'
+})
+export class Route1Component {
+
+}
